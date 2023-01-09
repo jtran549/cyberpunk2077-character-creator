2fba84113c27bfcb7ff0fcb5c6035abb704db58b
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2023-01-08 23:06:55 -0500

    Initial upload

---
+++ cyberpunk2077-character-creator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cyberpunk2077_character_creator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
