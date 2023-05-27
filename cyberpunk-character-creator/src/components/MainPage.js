8634287c8b1670ff928b7e601a68771376872c97
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2023-05-26 20:42:48 -0400

    Added step indicator at top of page

--- cyberpunk-character-creator/src/components/MainPage.js
+++ cyberpunk-character-creator/src/components/MainPage.js
@@ -1,9 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import React from 'react';
 import CreationStep from './CreationStep';
+import { Panel } from 'primereact/panel';
+
 
 function MainPage() {
   return (
-    <CreationStep />
+    <container>
+      <div className='col-md-5'>
+        <CreationStep></CreationStep>
+      </div>
+    </container>
+
   );
 }
 
--- cyberpunk-character-creator/src/index.js
+++ cyberpunk-character-creator/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primereact/resources/primereact.css';                       // core css                              // icons                                // css utility
+import 'primereact/resources/primereact.css';                       
 import './theme.css';
 import MainPage from './components/MainPage';
 
