5776ef60b8e7d0c7bf26ff8b5a2cb877a2a18e11
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2023-07-12 21:56:03 -0400

    Changed to typescript

---
+++ cyberpunk-character-creator/src/components/RolePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function RolePage() {
+    return (
+        <h1>ROLE STEP</h1>
+    )
+}
+        
--- cyberpunk-character-creator/src/index.tsx
+++ cyberpunk-character-creator/src/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'primereact/resources/primereact.css';                       
 import './theme.css';
-import MainPage from './components/MainPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <MainPage />
   </React.StrictMode>
 );
